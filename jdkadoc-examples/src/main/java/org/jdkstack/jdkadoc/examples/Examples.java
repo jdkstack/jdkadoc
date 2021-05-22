db47c834b77da5e80654ade1785e19cc435d242d
admin <www.sanshiwu.com@gmail.com>
2021-05-22 14:59:23 +0800

    feat(Project Core): Submit the project to GitHub for the first time
    Submit the project to GitHub for the first time.
    
    BREAKING CHANGE: Submit the project to GitHub for the first time.

---
+++ jdkadoc-examples/src/main/java/org/jdkstack/jdkadoc/examples/Examples.java
@@ -0,0 +1,23 @@
+package org.jdkstack.jdkadoc.examples;
+
+/**
+ * 例子.
+ *
+ * @author Admin
+ */
+public final class Examples {
+
+  private Examples() {
+    //
+  }
+
+  /**
+   * This is a method description.
+   *
+   * @param args 入口方法参数传递.
+   * @author Admin
+   */
+  public static void main(final String[] args) {
+    //
+  }
+}
